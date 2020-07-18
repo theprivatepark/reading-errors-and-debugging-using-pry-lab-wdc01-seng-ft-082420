8399fd180bd32102dcd7a4f8bfd14f10b690a24e
Anney Park <anneypark@gmail.com>
2020-07-18 01:41:32 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,4 +1,4 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
@@ -12,4 +12,5 @@ end
 def engage
   puts state_log(date)
   date = generate_star_date
+  date = generate_star_date()
 end
